dc5051fae93c4bc915871d1bbab78bccc3120870
Romashka <tankonatanke90@gmail.com>
2019-02-07 01:35:46 +0300

    popup img change

--- script.js
+++ script.js
@@ -3,22 +3,40 @@ var menu = document.getElementById("menu_wrapper")
 
 var closeBtn = document.getElementById("closebtn")
 var popup = document.getElementById("jspopup")
+var lenta = document.getElementById("lenta")
 
-
-var opening = true
 
 menuButton.onclick = function(){
-    if (opening == true){
-        menu.style.width = "420px"
-        opening = false
-    }else{
+    if (menu.style.width == "420px"){
         menu.style.width = "0"
-        opening = true
+    }else{
+        menu.style.width = "420px"
     }
 }
 
-closeBtn.onclick = function(){
+closeBtn.onclick = function(e){
     popup.style.display = "none"
 }
+
+function imagesClick(targetId){
+    var target = document.getElementById(targetId)
+    lenta.innerHTML = ""
+    var cloneAdv = target.cloneNode(true)
+    lenta.appendChild(cloneAdv)
+    popup.style.display = "flex"
+    var a = document.querySelectorAll("#lenta .fotos-preview > img")
+  
+    for(var i = 0; i < a.length; i ++){
+        a[i].onclick = prewImagesClick
+    }
+
+}
+
+function prewImagesClick(cant){
+    var fed = document.querySelector("#lenta .foto > img")
+    fed.src = cant.target.src
+}
+
+
 
 
