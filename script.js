968161ba5a86051eb80af334574f8cf0769b0e7b
Romashka <tankonatanke90@gmail.com>
2019-02-06 17:36:15 +0300

    popup btn close

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var menuButton = document.getElementById("menu_button")
 var menu = document.getElementById("menu_wrapper")
+
+var closeBtn = document.getElementById("closebtn")
+var popup = document.getElementById("jspopup")
+
+
 var opening = true
 
 menuButton.onclick = function(){
@@ -11,3 +16,9 @@ menuButton.onclick = function(){
         opening = true
     }
 }
+
+closeBtn.onclick = function(){
+    popup.style.display = "none"
+}
+
+
